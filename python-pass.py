ed388c6a4ef27d9132960da88a296d1d6b7b8c95
Noor AL-Huda Ali <89394843+DevNoora@users.noreply.github.com>
2021-09-30 17:43:54 +0300

    Add files via upload

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,34 @@
 
+#Laith code
+# class Solution:
 
-class Solution:
+#     @staticmethod
+#     def longest_palindromic(s: str) -> str:
+#         pass
 
-    @staticmethod
-    def longest_palindromic(s: str) -> str:
-        pass
+#Noora code
+class String(object):
+   def longestPalindrome(self, str):
+      dp = [[False for i in range(len(str))] for i in range(len(str))]
+      for i in range(len(str)):
+         dp[i][i] = True
+      max_length = 1
+      start = 0
+      for L in range(2,len(str)+1):
+         for i in range(len(str)-L+1):
+            end = i+L
+            if L==2:
+               if str[i] == str[end-1]:
+                  dp[i][end-1]=True
+                  max_length = L
+                  start = i
+            else:
+               if str[i] == str[end-1] and dp[i+1][end-2]:
+                  dp[i][end-1]=True
+                  max_length = L
+                  start = i
+      return str[start:start+max_length]
+stro1 = String()
+print("Enter a string:")
+str = input()
+print("Longest palindromic solution:",stro1.longestPalindrome(str))
